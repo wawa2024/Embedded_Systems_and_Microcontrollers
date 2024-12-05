d9edbb2630e49218e965d24aaeea6585ba472eb6
lamiika <47070356+lamiika@users.noreply.github.com>
2024-12-05 15:04:09 +0200

    Alarm handling prototype

--- software/nano_main_hub/nano_main_hub.ino
+++ software/nano_main_hub/nano_main_hub.ino
@@ -1,6 +1,7 @@
 #include "procSerial.h"
 #include "procLogin.h"
 #include "src/I2C_LCD/I2C_LCD.h"
+#include "alarmHandler.h"
 
 #define BAUD_RATE 115200
 
@@ -8,7 +9,7 @@ void setup() {
 
   lcd.init(); // init I2C lcd
   Serial.begin(BAUD_RATE,SERIAL_8N1); // init Serial
-
+  init_alarm();
 }
 
 void loop(void) {
---
+++ software/nano_main_hub/states.h
@@ -0,0 +1,6 @@
+volatile uint8_t armed_state = 0;
+volatile bool alarm_state = false;
+volatile long long alarm_time = 0;
+volatile bool login_state = false;
+
+uint8_t interrupt_pin = 2;
