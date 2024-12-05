f73814b2b08813f7f6293606a86930ee05a0119a
Juho Rantsi <juho.rantsi@tuni.fi>
2024-12-05 19:59:32 +0200

    timer header file updated

--- software/nano_main_hub/src/timer/timer.h
+++ software/nano_main_hub/src/timer/timer.h
@@ -2,6 +2,15 @@
 
 // Timer is initialized to generate 
 // a 1kHz overflow interrupt
+void timer1_init(void);
+
+
+// Sets the timer2 interrupt callback
+void timer1_setCallback( void (*userCallback)(void) );
+
+
+// Timer is initialized to generate 
+// a 1kHz overflow interrupt
 void timer2_init(void);
 
 
