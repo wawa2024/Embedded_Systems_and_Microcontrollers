a4286ce57dd3393790080b9bd9b31d39c3176210
Juho Rantsi <juho.rantsi@tuni.fi>
2024-12-06 16:16:32 +0200

    system files updated

--- software/nano_main_hub/system.cpp
+++ software/nano_main_hub/system.cpp
@@ -1,5 +1,18 @@
 #include "system.h"
 #include "src/I2C_LCD/I2C_LCD.h"
+#include "userPassword.h"
+
+static char initialPassword[PASSWORD_SIZE];
+
+void initialPassword_init( char* password )
+{   
+    if(p == NULL){ return; }
+
+    for(uint8_t i = 0; i < PASSWORD_SIZE; i++)
+    {
+        initialPassword[i] = password[i];
+    }
+}
 
 void system_init( void (*timer1_callback)(void), void (*timer2_callback)(void) )
 {
@@ -14,7 +27,12 @@ void system_init( void (*timer1_callback)(void), void (*timer2_callback)(void) )
     
     // read saved eeprom2 password and save to register
   // initPassword(); 
-  
+    
+    // Initialize the initial password variable
+    // so that when the device is reset the password
+    // can be compared with the current one
+    initialPassword_init( getPassword() );
+
     init_alarm();
 }
 
