e3ea1cb802f2024153ee636448d8b5524c4e2e9d
lamiika <47070356+lamiika@users.noreply.github.com>
2024-12-07 15:37:49 +0200

    Added buzzer countdown to alarm

--- software/nano_main_hub/alarmStates.cpp
+++ software/nano_main_hub/alarmStates.cpp
@@ -2,7 +2,13 @@
 
 volatile uint8_t armed_state = 1;
 volatile bool alarm_state = false;
-volatile long long alarm_time = 0;
 volatile bool login_state = false;
+bool alarm_activated = false;
+bool buzzer_state = false;
+volatile long long alarm_time = 0;
+const uint16_t alarm_delay = 6000;
+const uint16_t alert_cadence = 3000;
+uint8_t alarm_countdown = 5;
 
-uint8_t interrupt_pin = 2;
+const uint8_t interrupt_pin = 2;
+const uint8_t buzzer_pin = 8;
