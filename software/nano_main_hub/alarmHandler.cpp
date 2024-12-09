aa8a399bb04ae3b117131337d4fd508eabe9ce68
lamiika <47070356+lamiika@users.noreply.github.com>
2024-12-09 15:41:09 +0200

    Eeprom write alarm states

--- software/nano_main_hub/alarmHandler.cpp
+++ software/nano_main_hub/alarmHandler.cpp
@@ -5,10 +5,20 @@
 #include "userPassword.h"
 #include "procLogin.h"
 #include "guiMessage.h"
+#include "src/eeprom/eeprom.h"
 
 uint32_t buzzer_time = 0;
+const uint8_t alarm_state_address = 11;
+eeprom_t alarm_state_eeprom = { alarm_state_address, 0 };
+const uint8_t armed_state_address = 12;
+eeprom_t armed_state_eeprom = { armed_state_address, 0 };
 
 void init_alarm() {
+  eeprom_read(alarm_state_eeprom);
+  alarm_state = alarm_state_eeprom.data;
+  alarm_activated = alarm_state;
+  eeprom_read(armed_state_eeprom);
+  armed_state = armed_state_eeprom.data;
   pinMode(interrupt_pin, INPUT);
   pinMode(buzzer_pin, OUTPUT);
   attachInterrupt(digitalPinToInterrupt(interrupt_pin), register_alarm, RISING);
@@ -16,22 +26,24 @@ void init_alarm() {
 
 void armAlarm() {
   armed_state = 1;
+  armed_state_eeprom.data = armed_state;
+  eeprom_write(armed_state_eeprom);
   Serial.write("Alarm armed\n");
   lcdSuccess("Alarm armed");
 }
 
 void disarmAlarm() {
   armed_state = 0;
-  alarm_state = false;
-  alarm_activated = false;
-  buzzer_state = false;
-  digitalWrite(buzzer_pin, buzzer_state);
+  armed_state_eeprom.data = armed_state;
+  eeprom_write(armed_state_eeprom);
   Serial.write("Alarm disarmed\n");
   lcdSuccess("Alarm disarmed");
 }
 
 void disableAlarm() {
   alarm_state = false;
+  alarm_state_eeprom.data = alarm_state;
+  eeprom_write(alarm_state_eeprom);
   alarm_activated = false;
   buzzer_state = false;
   digitalWrite(buzzer_pin, buzzer_state);
@@ -47,6 +59,8 @@ void register_alarm() {
 
   if (armed_state == 1 && alarm_state == false) {
     alarm_state = true;
+    alarm_state_eeprom.data = alarm_state;
+    eeprom_write(alarm_state_eeprom);
     Serial.write("Opening detected, you have thirty seconds\n");
   }
 }
@@ -56,7 +70,7 @@ void trigger_alarm() {
 }
 
 bool poll_alarm_state() {
-  if (alarm_state == false) {}
+  if (alarm_state == false) {
     return false;
   }
 
